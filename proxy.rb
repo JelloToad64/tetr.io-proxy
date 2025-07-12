f3f041e276e91c123bdb481bac14934baa6d2e97
Toad64 <af4toad@gmail.com>
2025-07-11 23:35:50 -0400

    first commit

---
+++ proxy.rb
@@ -0,0 +1,16 @@
+require "sinatra"
+require "httparty"
+
+set :port, ENV.fetch("PORT", 4567)
+set :bind, "0.0.0.0"
+
+before do
+  content_type "application/json"
+  headers "Access-Control-Allow-Origin" => "*"
+end
+
+get "/tetrio/:username" do
+  response = HTTParty.get("https://ch.tetr.io/api/users/toad64")
+  status response.code
+  body response.body
+end
